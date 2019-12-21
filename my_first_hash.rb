3329c973bdf1ecc7d7ee2e083b4e8885e72cae9e
Asif Kamal <asif_kamal@hotmail.com>
2019-12-21 12:18:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,7 +21,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["whale bone corsets"]
 
 end
 
@@ -31,14 +31,12 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets" => 2, "gun powder" => 4]
 
 
 
-  # add 4 gun powder to the shipping_manifest hash below
 
 
-
-  # return the shipping_manifest hash below
+  shipping_manifest
 
 end
